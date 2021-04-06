9cc6e667cb2f97000cb5eb76d93176efac83094f
martim <a21805485@alunos.ulht.ot>
2021-04-06 16:24:13 +0100

    All done, ready to go!

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g6/DetalheTesteFragment.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g6/DetalheTesteFragment.kt
@@ -1,23 +1,47 @@
 package pt.ulusofona.deisi.a2020.cm.g6
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ceylonlabs.imageviewpopup.ImagePopup
 import kotlinx.android.synthetic.main.fragment_detalhe_teste.*
+
 
 class DetalheTesteFragment(var teste: TesteCovid) : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_detalhe_teste, container, false)
     }
 
     override fun onStart() {
         super.onStart()
-            detalheTesteLocal.text = teste.local
-            detalheTesteData.text = teste.data
-            detalheTesteResultado.text = teste.resultadoTesteCovid.toString()
+        detalheTesteLocal.text = teste.local
+        detalheTesteData.text = teste.data
+        if (teste.resultadoTesteCovid) {
+            detalheTesteResultado.text = getString(R.string.resultadoTestePositivo)
+        }else{
+            detalheTesteResultado.text = getString(R.string.resultadoTesteNegativo)
+        }
+        val imagePopup = ImagePopup(context)
+        imagePopup.setFullScreen(true)
+        imagePopup.setHideCloseIcon(true)
+        imagePopup.setImageOnClickClose(true)
+
+        imagePopup.initiatePopup(photo.getDrawable());
+
+        photo.setOnClickListener {
+            imagePopup.viewPopup();
+        }
+        photo_open.setOnClickListener {
+            imagePopup.viewPopup();
+        }
+
     }
 
 }
